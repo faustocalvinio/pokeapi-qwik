d7462a0559753890487ff331e49df330851e6353
Fausto Calviño <faustocalvino@outlook.com>
2023-10-17 14:44:26 +0200

    feat: pokemon image using signals

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,39 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const pokemonId = useSignal(1);
+ 
   return (
-    <>
-      <h1 class="text-5xl">hola mundo</h1>
-    </>
+    <div class="flex flex-col justify-center items-center">
+      <span class="text-2xl">Buscador Simple</span>
+      <span class="text-9xl">{pokemonId}</span>
+
+      {/* TODO CREAR IMAGEN */}
+      <img 
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId.value}.png`} 
+        alt="Pokemon Sprite" 
+        style={{width: "200px"}}
+        width={200}
+        height={200}
+      />
+
+      <div class="flex flex-row justify-between items-center w-[20%] mt-2 mb-2">
+        
+        <button onClick$={ ()=> pokemonId.value-- } class="btn btn-primary mt-2">Anterior</button>
+        <button onClick$={ ()=> pokemonId.value++ }  class="btn btn-primary">Siguiente</button>
+      </div>
+
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Poke Qwik",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Primera aplicaion en Qwik",
     },
   ],
 };
